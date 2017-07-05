673ce8caece80154892ee28be494c682cbabe7e2
Steven Hendricks <steven.hendricks@gmail.com>
2017-07-05 13:44:37 -0600

    appending fixed

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -16,7 +16,7 @@ $(document).ready(() => {
       // "image/webp" works in Chrome.
       // Other browsers will fall back to image/png.
       var b64image = canvas.toDataURL('image/png');
-      document.querySelector('#destinationImage').src = b64image;
+      // document.querySelector('#destinationImage').src = b64image;
       var base64Image = b64image.replace(/^data:image\/png;base64,/, "");
       // console.log(base64Image);
 
@@ -49,6 +49,45 @@ $(document).ready(() => {
                 score: (Math.floor((result.images[0].classifiers[0].classes[0].score) * 100) + ' %'),
                 url: res.data.link
               }
+
+              function displayImage(artist, score, url) {
+                var newCard =
+                `      <div class="row">
+                        <div class="col s12 m7">
+                          <div id="snapshotCard" class="card">
+                            <div class="card-image">
+                              <img src=" ` + artistResult.url + ` " class="snapshot" id="destinationImage">
+                              <span class="card-title"><font size="5" color="black"><font size="5" color="#538794">Saved</font></font></span>
+                            </div>
+                            <div class="card-content">
+                              <div class="row">
+                                <div class="col s12 m12 l6">
+                                  <h4><font color="#538794">Artist: </font></h4>
+                                </div>
+                                <div class="col s12 m12 l6">
+                                  <h4 id="artist"><font color="#538794">` + artistResult.artist + `</font></h4>
+                                </div>
+                              </div>
+                              <div class="row">
+                                <div class="col s12 m12 l6">
+                                  <h4><font color="#538794">Score: </font></h4>
+                                </div>
+                                <div class="col s12 m12 l6">
+                                  <h4 id="score"><font color="#538794">` + artistResult.score + `</font></h4>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="card-action">
+                              <a id="deleteButton" href="#">Delete</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>`
+                $("#savedImage").append(newCard);
+              }
+
+                displayImage()
+
               // JSON.stringify()
 
               // console.log(artistResult.artist);
@@ -58,90 +97,78 @@ $(document).ready(() => {
               // console.log(result.images[0].classifiers[0].classes[2].class);
               // console.log(result.images[0].classifiers[0].classes[2].score);
 
-              $('#artist').children().remove()
-              $('#score').children().remove()
+              // $('#artist').children().remove()
+              // $('#score').children().remove()
+              //
+              // if (artistResult.artist.length != undefined) {
+              //   $('#artist').append(`<h4>` + artistResult.artist + '</h4>')
+              //   $('#score').append('<h4>' + artistResult.score + '</h4>')
+              // } else {
+              //   $('#artist').append('<h4>' + 'Please try again' + '</h4>')
+              // }
+              //
+              // ///////SAVE BUTTON ////////
+              // $('#saveButton').click((event) => {
+              //   event.preventDefault();
+              //   //
+              //   var url = artistResult.url;
+              //   var artist = artistResult.artist;
+              //   var score = artistResult.score;
+              //
+              //   console.log("Saving here");
+              //   console.log(url);
+              //   console.log(artist);
+              //   console.log(score);
+              //
+              //   $.ajax({
+              //     url: '/save',
+              //     type: 'POST',
+              //     data: {
+              //       'imgur_url': res.data.link,
+              //       'artist': artistResult.artist,
+              //       'score': artistResult.score
+              //     },
+              //     success: function(result) {
+              //       // console.log('save sending');
+              //     }
+              //   })
 
-              if (artistResult.artist.length != undefined) {
-                $('#artist').append(`<h4>` + artistResult.artist + '</h4>')
-                $('#score').append('<h4>' + artistResult.score + '</h4>')
-              } else {
-                $('#artist').append('<h4>' + 'Please try again' + '</h4>')
-              }
+                // function displayImage(artist, score, url) {
+                //   var newCard =
+                //   `      <div class="row">
+                //           <div class="col s12 m7">
+                //             <div id="snapshotCard" class="card">
+                //               <div class="card-image">
+                //                 <img src=" ` + res.data.link + ` " class="snapshot" id="destinationImage">
+                //                 <span class="card-title"><font size="5" color="black"><font size="5" color="#538794">Saved</font></font></span>
+                //               </div>
+                //               <div class="card-content">
+                //                 <div class="row">
+                //                   <div class="col s12 m12 l6">
+                //                     <h4><font color="#538794">Artist: </font></h4>
+                //                   </div>
+                //                   <div class="col s12 m12 l6">
+                //                     <h4 id="artist"><font color="#538794">` + artistResult.artist + `</font></h4>
+                //                   </div>
+                //                 </div>
+                //                 <div class="row">
+                //                   <div class="col s12 m12 l6">
+                //                     <h4><font color="#538794">Score: </font></h4>
+                //                   </div>
+                //                   <div class="col s12 m12 l6">
+                //                     <h4 id="score"><font color="#538794">` + artistResult.score + `</font></h4>
+                //                   </div>
+                //                 </div>
+                //               </div>
+                //               <div class="card-action">
+                //                 <a id="deleteButton" href="#">Delete</a>
+                //               </div>
+                //             </div>
+                //           </div>
+                //         </div>`
+                //   $("#savedImage").append(newCard);
 
-              ///////SAVE BUTTON ////////
-              $('#saveButton').click((event) => {
-                event.preventDefault();
-                //
-                var url = artistResult.url;
-                var artist = artistResult.artist;
-                var score = artistResult.score;
 
-                console.log("Saving here");
-                console.log(url);
-                console.log(artist);
-                console.log(score);
-
-                $.ajax({
-                  url: '/save',
-                  type: 'POST',
-                  data: {
-                    'imgur_url': res.data.link,
-                    'artist': artistResult.artist,
-                    'score': artistResult.score
-                  },
-                  success: function(result) {
-                    // console.log('save sending');
-                  }
-                })
-
-                function displayImage(artist, score, url) {
-                  var newCard =
-                  `      <div class="row">
-                          <div class="col s12 m7">
-                            <div id="snapshotCard" class="card">
-                              <div class="card-image">
-                                <img src=" ` + res.data.link + ` " class="snapshot" id="destinationImage">
-                                <span class="card-title"><font size="5" color="black"><font size="5" color="#538794">Saved</font></font></span>
-                              </div>
-                              <div class="card-content">
-                                <div class="row">
-                                  <div class="col s12 m12 l6">
-                                    <h4><font color="#538794">Artist: </font></h4>
-                                  </div>
-                                  <div class="col s12 m12 l6">
-                                    <h4 id="artist"><font color="#538794">` + artistResult.artist + `</font></h4>
-                                  </div>
-                                </div>
-                                <div class="row">
-                                  <div class="col s12 m12 l6">
-                                    <h4><font color="#538794">Score: </font></h4>
-                                  </div>
-                                  <div class="col s12 m12 l6">
-                                    <h4 id="score"><font color="#538794">` + artistResult.score + `</font></h4>
-                                  </div>
-                                </div>
-                              </div>
-                              <div class="card-action">
-                                <a id="deleteButton" href="#">Delete</a>
-                              </div>
-                            </div>
-                          </div>
-                        </div>`
-                  $("#savedImage").append(newCard);
-
-              ////////////DELETE BUTTON////////////
-                  $('#deleteButton').click((event) => {
-                    event.preventDefault();
-
-                    console.log('Deleting here');
-                    $("#savedImage").children().remove();
-
-                  })
-
-                }
-                $("#savedImage").children().remove();
-                displayImage()
-              });
               // artistResult.artist = null
               // artistResult.score = null
               // res.data.link = null
@@ -152,7 +179,8 @@ $(document).ready(() => {
     }
   }
 
-  video.addEventListener('click', snapshot, false);
+  // video.addEventListener('click', snapshot, false);
+  captureButton.addEventListener('click', snapshot, false);
 
   // Not showing vendor prefixes or code that works cross-browser.
   navigator.getUserMedia({
